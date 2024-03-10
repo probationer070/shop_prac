d9cc806dd3114fbefbe9d916d0b7c3ee1756cce9
probationer070 <kogish13328@gmail.com>
2024-03-10 19:09:33 +0900

    alpha v0.02

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/index.html')
+@app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'GET':
+        return "GET으로 전달"
+    else:
+        return "POST로 전달"
 
 if __name__ == '__main__':
     app.run(debug=True)
