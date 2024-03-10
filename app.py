6d59e434a48052a3b79122b2a8d99bdf91e33570
probationer070 <kogish13328@gmail.com>
2024-03-10 22:43:40 +0900

    alpha v0.022

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return render_template("hello.html")
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-@app.route('/urltest')
-def url_test():
-    return redirect(url_for('daum'))
+@app.route('/dora')
+def myimg():
+    return render_template("myimg.html")
 
-@app.route('/method', methods=['GET', 'POST'])
-def method():
-    if request.method == 'GET':
-        # args_dict = request.args.to_dict()
-        # print(args_dict)
-        num = request.args["num"]
-        name = request.args.get("name")
-        return "GET으로 전달된 데이터({}, {})".format(num, name)
-    else:
-        num = request.form["num"]
-        name = request.form["name"]
-        return "POST로 전달된 데이터({}, {})".format(num, name)
+# @app.route('/method', methods=['GET', 'POST'])
+# def method():
+#     if request.method == 'GET':
+#         # args_dict = request.args.to_dict()
+#         # print(args_dict)
+#         num = request.args["num"]
+#         name = request.args.get("name")
+#         return "GET으로 전달된 데이터({}, {})".format(num, name)
+#     else:
+#         num = request.form["num"]
+#         name = request.form["name"]
+#         return "POST로 전달된 데이터({}, {})".format(num, name)
 
 
 if __name__ == '__main__':
-    with app.test_request_context():
-        print(url_for('daum'))
+    # with app.test_request_context():
+    #     print(url_for('static', filename='index.css'))
     app.run(debug=True)
