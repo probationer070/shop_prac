22b1b1b79f468ea05f17f81c8e0a8cdeb8d87492
probationer070 <kogish13328@gmail.com>
2024-03-10 19:47:37 +0900

    alpha v0.021

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/hello')
+def hello():
+    return render_template("hello.html")
+
+@app.route('/urltest')
+def url_test():
+    return redirect(url_for('daum'))
 
 @app.route('/method', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
-        return "GET으로 전달"
+        # args_dict = request.args.to_dict()
+        # print(args_dict)
+        num = request.args["num"]
+        name = request.args.get("name")
+        return "GET으로 전달된 데이터({}, {})".format(num, name)
     else:
-        return "POST로 전달"
+        num = request.form["num"]
+        name = request.form["name"]
+        return "POST로 전달된 데이터({}, {})".format(num, name)
+
 
 if __name__ == '__main__':
+    with app.test_request_context():
+        print(url_for('daum'))
     app.run(debug=True)
