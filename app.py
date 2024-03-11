ee8b153166f9ee4a83b95c40b1a70f13e6c7852c
probationer070 <kogish13328@gmail.com>
2024-03-11 21:32:44 +0900

    alpha v0.03

--- app.py
+++ app.py
@@ -5,25 +5,33 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/dora')
-def myimg():
-    return render_template("myimg.html")
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
-# @app.route('/method', methods=['GET', 'POST'])
-# def method():
-#     if request.method == 'GET':
-#         # args_dict = request.args.to_dict()
-#         # print(args_dict)
-#         num = request.args["num"]
-#         name = request.args.get("name")
-#         return "GET으로 전달된 데이터({}, {})".format(num, name)
-#     else:
-#         num = request.form["num"]
-#         name = request.form["name"]
-#         return "POST로 전달된 데이터({}, {})".format(num, name)
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'GET':
+        # args_dict = request.args.to_dict()
+        # print(args_dict)
+        user_id = request.args["usr-id"]
+        user_pw = request.args.get("usr-pw")
+
+        return "GET으로 전달된 데이터({}, {})".format(user_id, user_pw)
+    else:
+        user_id = request.form["usr-id"]
+        user_pw = request.form["usr-pw"]
+        with open("static/save.txt","w", encoding='utf-8') as f:
+            f.write("%s,%s" % (user_id, user_pw))
+        return "POST로 전달된 데이터({}, {})".format(user_id, user_pw)
+    
+    return render_template(["success.html"])
+
 
 
 if __name__ == '__main__':
-    # with app.test_request_context():
-    #     print(url_for('static', filename='index.css'))
     app.run(debug=True)
