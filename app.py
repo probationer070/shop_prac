3985bde836f5232e60ce310fbfe38db0e8fbdd0d
probationer070 <kogish13328@gmail.com>
2024-03-11 22:55:40 +0900

    alpha v0.024

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
+
+import json
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,11 +17,9 @@ def login():
 def signup():
     return render_template("signup.html")
 
-@app.route('/method', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
-        # args_dict = request.args.to_dict()
-        # print(args_dict)
         user_id = request.args["usr-id"]
         user_pw = request.args.get("usr-pw")
 
@@ -25,12 +27,22 @@ def method():
     else:
         user_id = request.form["usr-id"]
         user_pw = request.form["usr-pw"]
-        with open("static/save.txt","w", encoding='utf-8') as f:
-            f.write("%s,%s" % (user_id, user_pw))
+        
+        user_data = {
+            "user_name" : user_id,
+            "user_pass" : user_pw
+        }
+        with open("static/save.json","w", encoding='utf-8') as f:
+            json.dump(user_data, f, ensure_ascii=False)
         return "POST로 전달된 데이터({}, {})".format(user_id, user_pw)
     
     return render_template(["success.html"])
 
+@app.route('/getinfo')
+def getinfo():
+    with open("static/save.json", "r", encoding='utf-8') as file:
+        student = file.read().split(',')  # 쉽표로 잘라서 student 에 배열로 저장
+    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
 
 
 if __name__ == '__main__':
