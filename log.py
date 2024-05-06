6f4b716f5b8489c6ce8ad744cb3b0d982a2a1598
probationer070 <kogish13328@gmail.com>
2024-05-06 13:05:59 +0900

    alpha log

--- log.py
+++ log.py
@@ -7,7 +7,7 @@ logging.basicConfig(filename = "./log/test.log", level = logging.DEBUG)
 def log(request, message):
     log_date = get_log_date()
     log_message = "{0}/{1}/{2}".format(log_date, str(request), message)
-    # logging.info(log_message)
+    logging.info(log_message)
 
 def get_log_date():
     dt = datetime.datetime.now()
